a9f601479ee2021c7d126b9478ed3855cb35c148
Alan Gibson <alangibson27@gmail.com>
2015-07-29 22:45:41 +0100

    Added configurable options and a 'test' mode.

--- src/speccy.js
+++ src/speccy.js
@@ -1,38 +1,73 @@
-var borderleft = document.createElement("div")
-var borderright = document.createElement("div")
-var bordertop = document.createElement("div")
-var borderbottom = document.createElement("div")
+function add_borders() {
+    var borderleft = document.createElement("div")
+    var borderright = document.createElement("div")
+    var bordertop = document.createElement("div")
+    var borderbottom = document.createElement("div")
 
-document.body.appendChild(borderleft)
-document.body.appendChild(borderright)
-document.body.appendChild(bordertop)
-document.body.appendChild(borderbottom)
+    document.body.appendChild(borderleft)
+    document.body.appendChild(borderright)
+    document.body.appendChild(bordertop)
+    document.body.appendChild(borderbottom)
 
-function showBorder() {
-    var pattern
-    if (Math.floor((Math.random() * 10)) % 2 == 0) {
-        pattern = "speccy-header"
+    return {
+        borderleft: borderleft,
+        borderright: borderright,
+        bordertop: bordertop,
+        borderbottom: borderbottom
+    }
+}
+
+function run_with_options(options) {
+    var borders = add_borders()
+    var showTimeoutId
+
+    if (options.startAlways) {
+        show_borders(options, borders)
     } else {
-        pattern = "speccy-bytes"
+        showTimeoutId = setTimeout(show_borders, options.startTimeoutValue * 1000, options, borders)
     }
 
-    borderleft.setAttribute("class", pattern + " speccy-left")
-    borderright.setAttribute("class", pattern + " speccy-right")
-    bordertop.setAttribute("class", pattern + " speccy-top")
-    borderbottom.setAttribute("class", pattern + " speccy-bottom")
+    window.addEventListener("load", function() {
+        if (options.endImmediately) {
+            hide_borders(borders, showTimeoutId)
+        } else {
+            setTimeout(hide_borders, options.endTimeoutValue * 1000, borders, showTimeoutId)
+        }
+    })
 }
 
-function hideBorder() {
-    borderleft.remove()
-    borderright.remove()
-    bordertop.remove()
-    borderbottom.remove()
+function show_borders(options, borders) {
+    var pattern
+    switch (options.colours) {
+        case 'header':
+            pattern = 'speccy-header'
+            break
+
+        case 'bytes':
+            pattern = 'speccy-bytes'
+            break
+
+        case 'random':
+            if (Math.floor((Math.random() * 10)) % 2 == 0) {
+                pattern = "speccy-header"
+            } else {
+                pattern = "speccy-bytes"
+            }
+    }
+
+    borders.borderleft.setAttribute("class", pattern + " speccy-left")
+    borders.borderright.setAttribute("class", pattern + " speccy-right")
+    borders.bordertop.setAttribute("class", pattern + " speccy-top")
+    borders.borderbottom.setAttribute("class", pattern + " speccy-bottom")
 }
 
+function hide_borders(borders, showTimeoutId) {
+    if (showTimeoutId) {
+        clearTimeout(showTimeoutId)
+    }
 
-var showTimeoutId = setTimeout(showBorder, 1000)
-
-window.addEventListener("load", function() {
-    clearTimeout(showTimeoutId);
-    setTimeout(hideBorder, 2000);
-});
+    borders.borderleft.remove()
+    borders.borderright.remove()
+    borders.bordertop.remove()
+    borders.borderbottom.remove()
+}
